cbf4eb3c18e550d4c784766d8a1d1e4f8c2e90fe
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-19 17:15:12 +0100

    Add non existing endpoint error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,33 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import createHttpError, { isHttpError } from 'http-errors';
+import morgan from 'morgan';
 
 const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res, next) => {
 	res.json({ message: 'Hello World' });
 	next();
+});
+
+app.use((req, res, next) => {
+	next(createHttpError(404, 'Endpoint not found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+	console.error(error);
+	let errorMessage = 'An unknown error occurred';
+	let statusCode = 500;
+	if (isHttpError(error)) {
+		statusCode = error.status;
+		errorMessage = error.message;
+	}
+	res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
