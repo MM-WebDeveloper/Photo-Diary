68e49189a1b306ae4828ff8beb2bb55d62ece353
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-19 17:23:35 +0100

    Add connection to MongoDB Atlas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
+import mongoose from 'mongoose';
 import app from './app';
 import env from './util/validateEnv';
 
-app.listen(env.PORT, () => {
-	console.log(`Server running on port: ${env.PORT}`);
-});
+mongoose
+	.connect(env.MONGO_CONNECTION_STRING)
+	.then(() => {
+		console.log(`MongoDB Atlas connection was successful`);
+		app.listen(env.PORT, () => {
+			console.log(`Server running on port: ${env.PORT}`);
+		});
+	})
+	.catch(console.error);
