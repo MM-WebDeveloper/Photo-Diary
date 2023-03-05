67b26f1fd7b897ad2e7c6aaabc622dab17f90cc1
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-05 12:41:43 +0100

    Add lazy and suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,49 @@
 import './App.css';
+import {
+	createBrowserRouter,
+	Link,
+	Outlet,
+	RouterProvider,
+} from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const LoginPage = lazy(() => import('./pages/Login'));
 
 function App() {
-	return <p>Hello World</p>;
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Root />,
+			children: [
+				{
+					path: '/',
+					element: (
+						<Suspense fallback={<p>Loading...</p>}>
+							<LoginPage />
+						</Suspense>
+					),
+				},
+			],
+		},
+	]);
+	return (
+		<div className='App'>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
+
+const Root = () => {
+	return (
+		<>
+			<nav>
+				<Link to='/'>Home</Link>
+			</nav>
+			<div>
+				<Outlet />
+			</div>
+		</>
+	);
+};
 
 export default App;
