06beb16ad4519a136a2b18c74d9db0238a8b9528
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-08 18:18:07 +0100

    Refactor endpoint constants

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,5 +1,5 @@
-export const DASHBOARD = '/dashboard';
-export const LOGIN = '/';
+export const DASHBOARD = '/';
+export const LOGIN = '/login';
 export const REGISTER = '/register';
 export const PROFILE = '/p/:username';
 export const NOT_FOUND = '/404';
