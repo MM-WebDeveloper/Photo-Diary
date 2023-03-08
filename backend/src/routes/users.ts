7e4b1159042dc6331319ea35392d667cfb3b44f5
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-08 21:00:58 +0100

    Add authenticate user endpoint

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import * as UserController from '../controllers/users';
+import { authMiddleware } from '../middleware/auth';
 
 const router = express.Router();
 
+router.get('/', authMiddleware, UserController.AuthenticateUser);
 router.post('/register', UserController.Register);
 router.post('/login', UserController.Login);
 
