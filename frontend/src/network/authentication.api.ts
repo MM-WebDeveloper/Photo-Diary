988517c4b485b4e65c94be83ee292cfd289fb1f7
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-08 18:18:58 +0100

    Add the register endpoint fetch function

--- frontend/src/network/authentication.api.ts
+++ frontend/src/network/authentication.api.ts
@@ -1,6 +1,27 @@
 import { User } from '../models/user';
 import { fetchData } from './fetch';
 
+export interface RegisterCredentials {
+	username: string;
+	fullName: string;
+	email: string;
+	password: string;
+}
+
+export const registerUser = async (
+	credentials: RegisterCredentials
+): Promise<User> => {
+	const res = await fetchData('/api/users/register', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(credentials),
+	});
+
+	return res.json();
+};
+
 export interface LoginCredentials {
 	usernameOrEmail: string;
 	password: string;
