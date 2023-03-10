cb2ee61f14e2abf43cee6b3f230d26840eaa41f1
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-10 16:25:12 +0100

    Refactor routes constants

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -2,4 +2,4 @@ export const DASHBOARD = '/';
 export const LOGIN = '/login';
 export const REGISTER = '/register';
 export const PROFILE = '/p/:username';
-export const NOT_FOUND = '/404';
+export const NOT_FOUND = '/*';
