dd4bb4a1293797591b9f644e3850bf156a189ab1
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-10 18:23:51 +0100

    Add useeffect to not found

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from 'react';
+
 const NotFound = () => {
+	useEffect(() => {
+		document.title = 'Not Found';
+	}, []);
 	return <div>404 | Page Not Found</div>;
 };
 export default NotFound;
